20819042328c93c54c904632c43a91f9c144229c
FelipeSantana <felipesantana27@gmail.com>
2024-11-27 22:59:34 -0300

    Criando a tabela no para exibir usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,19 +11,33 @@
             <div class="p-6 text-gray-900 dark:text-gray-100">
               <p class="mb-4">Olá <strong>{{Auth::user()->name}}</strong></p>
             </div>
-
-            <div class="p-6 text-gray-900">
                 <table class="table-auto w-full">
-                    <thead class="text-left bg-gray-800">
+                    <thead class="font-semibold text-md text-gray-800 dark:text-gray-200 leading-tight">
                         <tr>
-                            <th class="text-center">Nivel</th>
+                            <th class="text-center p-2">Nivel</th>
                             <th class="p-2">Nome</th>
                             <th class="p-2">Email</th>
                             <th class="p-2">Data de Cadastro</th>
                             <th>Ações</th>
                         </tr>
                     </thead>
+                <tbody>
+                 @foreach($users as $user)
+                     <tr class="hover:bg-gray-200 text-gray-800 dark:text-gray-200 leading-tight">
+                         <td class="text-center">icone</td>
+                         <td class="text-center p-2">{{$user->name}}</td>
+                         <td class="text-center p-2">{{$user->email}}</td>
+                         <td class="text-center p-2">{{$user->created_at}}</td>
+                         <td class="text-center p-2">
+                             <a href="" class="href">Editar</a>
+                         </td>
+                     </tr>
+                 @endforeach
+                </tbody>
                 </table>
+            <div class=" mb-4  mt-4 mx-4 text-gray-800 dark:text-gray-200 leading-tight rounded-lg">
+                {{$users->links()}}
+            </div>
             </div>
 
         </div>
